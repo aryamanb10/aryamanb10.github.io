3957251e3627202bb72efc07761cc5dd692d05dd
aryamanb10 <bajajaryaman@gmail.com>
2017-04-01 14:39:47 +0530

    Create second.js

---
+++ p5/second.js
@@ -0,0 +1,81 @@
+function setup() { 
+  createCanvas(500, 500);
+} 
+var q=150
+var y=0
+var x=150
+function draw() { 
+  background(100);
+	
+arc(x,q,50,50,PI/4,-PI/4,PIE)
+	if (y>20) {}
+	else {ellipse(170,150,5,5)
+	arc(x,q,50,50,PI/4,-PI/4,PIE)			
+			 }
+	
+	if (y>40) {}
+	else {ellipse(190,150,5,5)
+			 
+			 }
+	
+	if (y>60) {}
+	else {ellipse(210,150,5,5)
+			 
+			 }
+	if (y>80) {}
+	else {ellipse(230,150,5,5)
+			 
+			 }
+	if (y>100) {}
+	else {ellipse(250,150,5,5)
+			 
+			 }
+	if (y>120) {}
+	else {ellipse(270,150,5,5)
+			 
+			 }
+	if (y>140) {}
+	else {ellipse(290,150,5,5)
+			 
+			 }
+	if (y>160) {}
+	else {ellipse(310,150,5,5)
+			 
+			 }
+	if (y>180) {}
+	else {ellipse(330,150,5,5)
+			 
+			 }
+	if (y>200) {}
+	else {ellipse(350,150,5,5)
+			 
+			 }
+	
+	y=y+1
+	if (y>200) {
+	 q=q+1}
+	else {x=x+1}
+	if (y>220) {}
+	else {ellipse(350,170,5,5)}
+				if (y>240) {}
+	else {ellipse(350,190,5,5)}
+	if (y>260) {}
+	else {ellipse(350,210,5,5)}
+	if (y>280) {}
+	else {ellipse(350,230,5,5)}
+	if (y>300) {}
+	else {ellipse(350,250,5,5)}
+	if (y>320) {}
+	else {ellipse(350,270,5,5)}
+	if (y>340) {}
+	else {ellipse(350,290,5,5)}
+	if (y>360) {}
+	else {ellipse(350,310,5,5)}
+	if (y>380) {}
+	else {ellipse(350,330,5,5)}
+	if (y>380) {
+	
+							q=q-1
+							x=x-1}
+}
+
